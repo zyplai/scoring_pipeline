1ab6041bf468e5e6578e00ed3ed2a044ccd61592
s-masrur <masrur.sobirov@gmail.com>
2023-11-03 02:52:28 +0500

    added adversarial validation

--- commands.py
+++ commands.py
@@ -9,6 +9,7 @@ from data_prep.normalize_raw_data import define_target, map_col_names
 from data_prep.sample_prep import prepare_main_sample
 from features.macro_features import prepare_macro_features
 from models.train import fit, predict
+from validation.adversarial_val import perform_adv_val
 from utils.basic_utils import read_file
 
 warnings.filterwarnings("ignore")
@@ -43,12 +44,22 @@ def enrich_with_features(df: pd.DataFrame):
 
 
 def run_scoring_pipe():
-    sample = preprocess_raw_sample()
-    clean_sample = prepare_main_sample(
-        df=sample, test_size=settings.TRAIN_SAMPLE_PROPS.test_size
-    )
-    fit(clean_sample)
-    predict(clean_sample)
+    # sample = preprocess_raw_sample()
+    # clean_sample = prepare_main_sample(
+    #     df=sample, test_size=settings.TRAIN_SAMPLE_PROPS.test_size
+    # )
+    # fit(clean_sample)
+    # predict(clean_sample)
+    
+    # train_data = read_file(settings.TRAIN_SAMPLE_PROPS.train_sample_path)
+    # blind_data = read_file(settings.BLIND_SAMPLE_PROPS.blind_path)
+    
+    train_data = pd.read_excel('C:\\Users\\masrur\\code\\finca-guatemala\\data\\train_data.xlsx')
+    blind_data = pd.read_excel('C:\\Users\\masrur\\code\\finca-guatemala\\data\\blind_data.xlsx')
+                                      
+    adv_auc = perform_adv_val(train_data, blind_data)
+    
+    print(adv_auc)
 
 
 if __name__ == '__main__':
